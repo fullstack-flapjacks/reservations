e602fab09691e5b2379228a775dbee2abf158036
zaid <zaidhusseini@gmail.com>
2018-02-22 11:11:36 -0800

    Initialize booking count seed data for each restaurant

--- database/dataGenerator.js
+++ database/dataGenerator.js
@@ -3,13 +3,13 @@ Data Generator File
 This js file initializes random restaurant availability data that will be seeded into the DB
 */
 
-var data = [];
+var availabilityData = [];
+var bookingData = [];
 
 //generate a random number between two values inclusive
 function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive 
 }
-
 
 //generates random minutes between 0 - 45
 var randomMinutes = function randomMinutes() {
@@ -23,6 +23,13 @@ var randomMinutes = function randomMinutes() {
   return minutes[random];
 }
 
+var initializeBookings = function(restaurant, day) {
+  restaurant.bookings.push({
+    day: day,
+    bookings_count: 0
+  });
+}
+
 var generateRandomTimeSlots = function generateRandomTimeSlots(restaurant, day, numSlots) {
   for (var i = 0; i < numSlots; i++) {
     restaurant.availability.push({
@@ -33,7 +40,7 @@ var generateRandomTimeSlots = function generateRandomTimeSlots(restaurant, day, 
   }
 }
 
-var generateRandomAvailabilityData = function generateRandomAvailabilityData() {
+var generateRandomRestaurantData = function generateRandomRestaurantData() {
   //initialize 200 restaurant ids
   for (var i = 0; i < 200; i++) {
     //instantiate empty restaurant object
@@ -41,16 +48,25 @@ var generateRandomAvailabilityData = function generateRandomAvailabilityData() {
       id: i,
       availability: []
     }
-    //Generate Time Slots for each of 30 days
-    for (var day = 1; day <= 30; day++) {
-      generateRandomTimeSlots(restaurant, day, randomInt(0, 7)); //Randomize number of available slots per day
+    var bookings = {
+      id: i,
+      bookings: []
     }
-    data.push(restaurant); //push restaurant object into raw data array
+    //Generate Time Slots for each of 30 days
+    for (var day = 1; day <= 31; day++) {
+      generateRandomTimeSlots(restaurant, day, randomInt(0, 7)); //Randomize number of available slots per day
+      initializeBookings(bookings, day);
+    }
+    availabilityData.push(restaurant); //push restaurant object into raw data array
+    bookingData.push(bookings);
   }
 }
 
-generateRandomAvailabilityData();
+generateRandomRestaurantData();
 
-console.log(data[1]);
+console.log(availabilityData[1]);
+console.log(bookingData[2]);
 
-module.exports = data;
+
+
+module.exports.availability = availabilityData;
