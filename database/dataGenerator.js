bcaee644bd0ce4515efe3f500e186bf9397b8646
zaid <zaidhusseini@gmail.com>
2018-02-21 20:16:46 -0800

    Set up seed & data generation files to generate random data

---
+++ database/dataGenerator.js
@@ -0,0 +1,15 @@
+/*
+Data Generator File
+This js file initializes random restaurant availability data that will be seeded into the DB
+*/
+
+var data = [];
+
+//initialize 200 restaurant ids
+for (var i=0; i<200; i++){
+  data[i].push({id:i});
+}
+
+//generate random availability slots
+
+
--- database/seed.js
+++ database/seed.js
@@ -1,1 +1,14 @@
-  //seed data
+const mongoose = require('mongoose'); // include DB object
+const seedData = require('dataGenerator');
+
+mongoose.connect('mongodb://localhost/restaurants');
+
+const availabilitySchema = mongoose.Schema({
+  id: Number,
+  availability: [{
+    day: Number,
+    time: Number,
+  }],
+});
+
+let Availability = mongoose.model('Availability', availabilitySchema);
