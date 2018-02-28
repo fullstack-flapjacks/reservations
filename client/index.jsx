7b661b021db507a8c59df3879c369a8d6ecd2788
zaid <zaidhusseini@gmail.com>
2018-02-28 12:21:37 -0800

    Implement front end component, party size component and basic CSS file

--- client/index.jsx
+++ client/index.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+import PartySelector from './components/PartySelector';
 import $ from 'jquery';
 
 window.$ = window.jQuery = $;
 
 class App extends React.Component {
   render () {
-    return <p> Hello World! This is the reservation component</p>;
+    return (<div className="container reservation-widget">
+              <h5 className="header">Make a Reservation</h5>
+                <form>
+                  <div class="form-group">
+                    <PartySelector />
+                  </div>
+                </form>
+            </div>);
   }
 }
 
