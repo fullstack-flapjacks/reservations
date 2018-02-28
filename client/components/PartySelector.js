fe74b474a734600cf002c708db690a616ad9911b
zaid <zaidhusseini@gmail.com>
2018-02-28 12:21:37 -0800

    Implement front end component, party size component and basic CSS file

---
+++ client/components/PartySelector.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+var PartySelector = ()=>
+         (<div>
+            <label for="exampleFormControlSelect1" className="form-party-size">Party Size</label>
+              <select className="form-control party-size" id="exampleFormControlSelect1">
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+              </select>
+          </div>);
+
+
+export default PartySelector;
