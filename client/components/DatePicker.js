e7b7f12bf25abca4cae957c33da46c7ef3ae98d5
zaid <zaidhusseini@gmail.com>
2018-02-28 12:42:15 -0800

    Create DatePicker component stub

---
+++ client/components/DatePicker.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+var DatePicker = ()=>{
+
+  return (
+
+
+
+    )
+
+}
+
+export default DatePicker;
--- client/components/PartySelector.js
+++ client/components/PartySelector.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
-var PartySelector = ()=>
-         (<div>
-            <label for="exampleFormControlSelect1" className="form-party-size">Party Size</label>
-              <select className="form-control party-size" id="exampleFormControlSelect1">
-                <option>1</option>
-                <option>2</option>
-                <option>3</option>
-                <option>4</option>
-                <option>5</option>
-              </select>
-          </div>);
+var PartySelector = ()=>{
+  var options = [];
+  for(var i=1; i<= 20; i++){
+    options.push(i);
+  }
 
+  return(<div>
+          <label for="party-size" className="form-party-size">Party Size</label>
+            <select className="custom-select party-size" id="party-size">
+              {options.map((val)=>(<option value={val}> {val} </option>))}
+            </select>
+        </div>);
+
+}
 
 export default PartySelector;
