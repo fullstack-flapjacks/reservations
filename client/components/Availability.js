c75a6118f5bd40deda1337fb87dc2b1b75e78b82
zaid <zaidhusseini@gmail.com>
2018-03-01 15:07:46 -0800

    Convert military times to user friendly AM/PM format in reservation widget

--- client/components/Availability.js
+++ client/components/Availability.js
@@ -1,20 +1,45 @@
 import React from 'react';
 
+const militaryTimeConverter = (times)=>{
+  
+  return times.map((time)=>{
+    var ampm;
+    var stringTime;
+
+    //convert times to AM/PM string based on time of day
+    if (time.hour > 12){
+      ampm = time.hour-12;
+      stringTime = String(ampm) + ':' + String(time.minute || '00') + ' PM';
+
+    } else if (time.hour < 12 && time.hour !== 0){
+      ampm = time.hour;
+      stringTime = String(ampm) + ':' + String(time.minute || '00') + ' AM';
+
+    } else if (time.hour === 12 || time.hour === 0){
+      ampm = 12;
+      stringTime = String(ampm) + ':' + String(time.minute || '00') + (time.hour ? ' PM' : ' AM');
+    }
+   
+   //add property to each object
+   time.time = stringTime;
+
+   return time; //return updated time object
+  });
+
+}
+
+
 var Availability = (props)=>{
   
-  const available = props.tables.length > 0; //check if there are available tables
-
-  if(available){
-
-
-  }
+  let available = props.tables.length > 0; //check if there are available tables
 
   if (available){
 
-    return (<div className="slots">
-             {props.tables.map(table =><div className="table"> {`${table.hour}:${table.minute}`} </div>)}
-           </div>)
+    available = militaryTimeConverter(props.tables); //add AM/PM strings for user friendly display on reservation widget
 
+    return (<div className="slots">
+             {props.tables.map(table =><div className="table"> {`${table.time}`} </div>)}
+           </div>)
   }
 
   return (<div className="slots"> Sorry! No tables available :( </div>)
