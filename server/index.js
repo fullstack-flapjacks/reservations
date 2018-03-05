0f8cc835bc49f4971d533e4990c9f615331b05e9
zaid <zaidhusseini@gmail.com>
2018-03-05 15:54:36 -0800

    Minor fix to make service work with Proxy Server

--- server/index.js
+++ server/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 
 const app = require('./app')
 
-const PORT = process.env.PORT || 2002;
+const PORT = process.env.PORT || 2003;
 
 app.listen(PORT, ()=> console.log('Listening on Port ', PORT));
